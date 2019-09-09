5282288fbba252d60dbc596e14f911ff7206e396
Silver92 <wen.xiao@mail.mcgill.ca>
2019-09-09 17:45:40 -0400

    Made a TODO list.

---
+++ Source/TODO.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    TODO.h
+    Created: 9 Sep 2019 5:32:35pm
+    Author:  Silver
+
+  ==============================================================================
+*/
+
+#pragma once
+
+/*
+ 1. Add the preset manager
+ 2. Add the LookAndFeel
+ 3. Add the VU meter
+ 4. Decorate the UI (Searching new UI patterns)
+*/
