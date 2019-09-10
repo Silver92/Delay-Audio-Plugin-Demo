6ac488272276c65b45a894438766834e17ebd1b6
Silver92 <wen.xiao@mail.mcgill.ca>
2019-09-09 22:05:29 -0400

    Added some potential improvement in TODO list.

--- Source/TODO.h
+++ Source/TODO.h
@@ -11,8 +11,12 @@
 #pragma once
 
 /*
+ 
+ When delay == 0, the delay line is not stable. possible cause of the point precision.
+ 
  1. Add the preset manager
  2. Add the LookAndFeel
  3. Add the VU meter
  4. Decorate the UI (Searching new UI patterns)
+ 5. Possible refurbishing: phaser
 */
